fbf001f5e7e54fca6f7a7c3f8b7337074a4beb1a
Alexander Pahn <pain.god18@gmail.com>
2021-01-24 11:29:08 +0100

    Initial Set up

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import {
+  Plugins,
+  CameraResultType,
+  CameraSource,
+  Capacitor,
+} from '@capacitor/core';
+const { Camera } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  async onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera')) {
+      return;
+    }
+    // unexpected behaviour seems to appear while calling Camera.getPhoto
+    const capturedImage = await Camera.getPhoto({
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera,
+      quality: 100,
+      correctOrientation: true,
+    }).catch(error => {
+      console.log(error);
+      return null;
+    });
+    console.dir(capturedImage);
+  }
 }
